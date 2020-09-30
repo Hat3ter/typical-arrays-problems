cf9127c17209e01b6a456108bf3b30348216b152
Alexandr Pavliychenkov <a.pavliychenkov@gmail.com>
2020-09-30 20:45:47 +0300

    feat: added functions:   - min   - max   - avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (getZeroIfNegative(array)) return 0;
+    return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (getZeroIfNegative(array)) return 0;
+    return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (getZeroIfNegative(array)) return 0;
+    const sum = [...array].reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+    return sum / array.length;
+}
+
+
+const getZeroIfNegative = (input) => {
+    return isBlank(input) || isUndefined(input) || isZeroLength(input)
+}
+
+const isBlank = (input) => {
+    return input === null || input === ''
+}
+const isUndefined = (input) => {
+    return input === undefined
+}
+const isZeroLength = (input) => {
+    return [...input].length === 0
 }
